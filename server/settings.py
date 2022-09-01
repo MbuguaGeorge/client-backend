70c554c65a1bc5ef77bfa4aaee7beabc5f753335
Sceptre <mbuguag026@gmail.com>
2022-09-01 16:10:21 +0300

    deployment

--- server/settings.py
+++ server/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
