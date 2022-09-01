bf7d169ccb6bfaf3073f5c4c40a4dcef8f8a3c51
Sceptre <mbuguag026@gmail.com>
2022-09-01 23:47:42 +0300

    deploy

--- server/settings.py
+++ server/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'freelance',
         'USER':'postgres',
-        'PASSWORD':'dbnue540an3kf2',
+        'PASSWORD':'George026#',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
