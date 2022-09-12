225dc5f84c763de5d8c85a5d08e3c333bd506189
Sceptre <mbuguag026@gmail.com>
2022-09-12 19:20:55 +0300

    environment variables

--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
