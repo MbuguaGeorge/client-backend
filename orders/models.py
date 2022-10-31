e279031fdfb7cba423e04c14dc3a3a8cea7c735a
Sceptre <mbuguag026@gmail.com>
2022-10-31 17:10:53 +0300

    migrations

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class Academic_Writing(models.Model):
     charts = models.IntegerField(default=0, null=True, blank=True)
     slides = models.IntegerField(default=0, null=True, blank=True)
     instructions = models.TextField(null=True, blank=True)
-    instruction_file = models.FileField(null=True, blank=True)
+    instruction_file = models.FileField(upload_to='InstructionFiles/', null=True, blank=True)
     paper_format = models.CharField(max_length=200, null=True, blank=True)
     references = models.IntegerField(default=0, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
